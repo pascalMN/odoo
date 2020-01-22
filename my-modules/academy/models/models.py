d56c357e90a76446ea309f2e891f5a9ac4f37de7
m.nikiel <m.nikiel@samsung.com>
2020-01-22 16:26:42 +0100

    Relational fields
    http://www.odoo.com/documentation/12.0/howtos/backend.html#relational-fields

---
+++ my-modules/academy/models/models.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class Teachers(models.Model):
+    _name = 'academy.teachers'
+
+    name = fields.Char()
+    biography = fields.Html()
+
+    course_ids = fields.One2many('product.template', 'teacher_id', string="Courses")
+
+
+class Courses(models.Model):
+    _inherit = 'product.template'
+
+    teacher_id = fields.Many2one('academy.teachers', string="Teacher")
