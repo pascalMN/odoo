83ee72ff89242e1f17fc55a89f249c67b943e71d
Mateusz Nikiel <pascal.nikiel@gmail.com>
2020-02-25 20:15:45 +0100

    Support for web element

--- my-modules/academy/static/src/app/app.module.ts
+++ my-modules/academy/static/src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {Injector, NgModule} from '@angular/core';
+import {createCustomElement} from '@angular/elements';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
+  entryComponents: [
+    AppComponent
+  ],
   imports: [
     BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  ]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) { }
+
+  ngDoBootstrap() {
+    const element = createCustomElement(AppComponent, {injector: this.injector});
+    customElements.define('test-element', element);
+  }
+}
