323c4a6d3a8a76e8155ef3135ac8aa87f7f27c18
m.nikiel <m.nikiel@samsung.com>
2020-02-26 11:04:49 +0100

    Elements development scripts

---
+++ my-modules/academy/static/gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var watch = require('gulp-watch');
+
+var production = process.argv.includes('--prod');
+var files = [
+    './dist/static/main.js',
+    './dist/static/polyfills.js',
+    './dist/static/runtime.js'
+];
+
+if (!production) {
+    files.push('./dist/static/vendor.js');
+}
+
+function bundle() {
+    return gulp.src(files)
+        .pipe(concat('elements.js'))
+        .pipe(gulp.dest('./dist/static'))
+}
+
+gulp.task('bundle', function () {
+    return bundle();
+});
+
+gulp.task('bundle:watch', function () {
+    return watch(files, { allowEmpty: true }, bundle);
+});
